a1bf8b7c8ce07383716ed88631606c3c0e6d2b5b
Christy Chen <christy.w.chen@gmail.com>
2022-01-24 00:13:14 -0800

    minor updates to css shape sizes and colors.

--- src/components/Splash/index.js
+++ src/components/Splash/index.js
@@ -8,38 +8,39 @@ function Splash() {
     return (
         <>
             <div id='splash__main'>
-                <div id='splash__title'>Hi! I'm Christy.</div>
-                <div id='splash__desc'>
-                    <p>
-                        I tell stories through code.
-                    </p>
-                    <p>
-                        Let's build something to make the world a better place.
-                    </p>
+                <div id='splash__text'>
+                    <div id='splash__title'>Hi! I'm Christy.</div>
+                    <div id='splash__desc'>
+                        <p>
+                            I tell stories through code.
+                        </p>
+                        <p>
+                            Let's build something to make the world a better place.
+                        </p>
+                    </div>
+                    <div id='splash__cta'>
+                        <Link to='/home'>Enter</Link>
+                    </div>
                 </div>
-                <div id='splash__cta'>
-                    <Link to='/home'>Enter</Link>
+                <div id='splash__lines'>
+                    <div className='splash__line'></div>
+                    <div className='splash__line'></div>
+                    <div className='splash__line'></div>
                 </div>
-            </div>
-            <div id='splash__social'>
-                <a href='https://github.com/christywchen/' target="_blank" rel="noreferrer noopener">
-                    <img className='splash__social--icon' src={github} />
-                </a>
-                <a href='https://www.linkedin.com/in/christy-chen/' target="_blank" rel="noreferrer noopener">
-                    <img className='splash__social--icon' src={linkedin} />
-                </a>
+                <div id='splash__social'>
+                    <a href='https://github.com/christywchen/' target="_blank" rel="noreferrer noopener">
+                        <img className='splash__social--icon' src={github} />
+                    </a>
+                    <a href='https://www.linkedin.com/in/christy-chen/' target="_blank" rel="noreferrer noopener">
+                        <img className='splash__social--icon' src={linkedin} />
+                    </a>
+                </div>
             </div>
             <div id='splash__cylinders'>
                 <div className='splash__cylinder splash__cylinder--1'></div>
                 <div className='splash__cylinder splash__cylinder--2'></div>
                 <div className='splash__cylinder splash__cylinder--3'></div>
             </div>
-            <div id='splash__lines'>
-                <div className='splash__line'></div>
-                <div className='splash__line'></div>
-                <div className='splash__line'></div>
-            </div>
-            <div id='splash__circle'></div>
             <div id='splash__squiggly'>
                 <div className='splash__squiggly--ellipse-1'></div>
                 <div className='splash__squiggly--ellipse-1 splash__squiggly--ellipse-2'></div>
@@ -47,7 +48,10 @@ function Splash() {
             <div id='splash__triangle--1'></div>
             <div id='splash__triangle--2'></div>
             <div id='splash__triangle--3'></div>
-            <div id='splash__vertbar'></div>
+            <div id='splash__verticals'>
+                <div id='splash__vertbar'></div>
+                <div id='splash__circle'></div>
+            </div>
         </>
     )
 }
