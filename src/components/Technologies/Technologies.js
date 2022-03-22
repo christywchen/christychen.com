47f6dbb0603dfcc05671de8756fb9309e0167dec
Christy Chen <christy.w.chen@gmail.com>
2022-03-21 17:53:49 -0700

    added responsive media queries for technologies panel.

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -5,7 +5,9 @@ function Technologies() {
         <>
             <section id='technologies__container'>
                 <div className='technologies__header'>Technologies</div>
-                Some of the technologies I've been working with lately...
+                <span className='technologies__desc'>
+                    Some of the technologies I've been working with lately...
+                </span>
                 <div className='technologies__container--content'>
                     <div>
                         <ul className='technologies__list'>
@@ -13,16 +15,21 @@ function Technologies() {
                             <li>Express.js</li>
                             <li>Node.js</li>
                             <li>Sequelize</li>
-                            <li>React / Redux</li>
-                            <li>HTML5 & CSS3</li>
-                        </ul>
-                    </div>
-                    <div>
-                        <ul className='technologies__list'>
                             <li>Python</li>
                             <li>Flask</li>
                             <li>SQLAlchemy</li>
                             <li>PostgreSQL</li>
+                        </ul>
+                    </div>
+                    <div>
+                        <ul className='technologies__list'>
+                            <li>React / Redux</li>
+                            <li>HTML5</li>
+                            <li>CSS3</li>
+                            <li>react-spring</li>
+                            <li>SocketIO</li>
+                            <li>AWS EC2</li>
+                            <li>AWS S3</li>
                         </ul>
                     </div>
                 </div>
