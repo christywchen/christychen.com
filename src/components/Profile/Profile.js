0aa3e37c04d04223fd25df69a4dccac9e0a874a2
Christy Chen <christy.w.chen@gmail.com>
2022-03-23 18:53:12 -0700

    bio description update.

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -13,7 +13,7 @@ function Profile() {
                                 As a child of the West Coast, I've always had a mentality that matched the lifestyle. Rather than getting lost in the daily grind, I prefer seeking joy in the meaningful experiences of everyday life. For a while, I transformed my affinity for mindful living into a more complex skill: Crafting integrated online marketing campaigns that help brands tell meaningful, authentic stories.
                             </p>
                             <p>
-                                These days, I still tell stories and create, but my means has shifted.
+                                These days, I still tell stories and still create, but my means has shifted.
                             </p>
                             <p>
                                 As a software engineer, my toolkit now includes a wide breadth of technologies and skills. I'm also leveraging my past experience in digital marketing, ecommerce, and studying user behavior as I seek to build full-stack applications that bring joy to not only myself, but others as well.
